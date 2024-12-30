42adf5c285c1949d4a2da2971fea2238a27f123f
EvertonWallace <everton542@hotmail.com>
2024-12-30 00:22:42 -0300

    remocao do navbar da pagina home de adm

--- src/components/admin/AdminHome.jsx
+++ src/components/admin/AdminHome.jsx
@@ -1,13 +1,10 @@
 import { Link } from 'react-router-dom';
 import '../../styles/home_admin.css';
-import OndaNavbar from '../navbar/OndaNavbar.jsx';
 
 export default function AdminHome() {
   return (
     <>
       {/*<Header></Header>*/}
-      <OndaNavbar></OndaNavbar>
-
       <div className="container">
         <section className="section1">
           <button className="button">
