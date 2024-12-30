cdb9574929229a79a0482dc8d99975d3c3b8d2ba
EvertonWallace <everton542@hotmail.com>
2024-12-30 00:23:28 -0300

    inclusao de botao para acessar login de administrador

--- src/components/navbar/OndaNavbar.jsx
+++ src/components/navbar/OndaNavbar.jsx
@@ -1,18 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../../styles/global.css'; // Certifique-se de importar o arquivo CSS
+import '../../styles/navbar.css';
 
 function OndaNavbar() {
   return (
     <Navbar expand="lg" className="onda-navbar">
       <Container>
-        <Navbar.Brand href="/home" className="navbar-brand">
+        <Navbar.Brand className="navbar-brand">
           Onda
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto navbar-item">
+          <Nav className="me-auto navbar-links">
             <Nav.Link href="/depoimentos" className="navbar-item">
               Depoimentos
             </Nav.Link>
@@ -21,6 +21,11 @@ function OndaNavbar() {
             </Nav.Link>
             <Nav.Link href="#link" className="navbar-item">
               Serviços UFBA
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/login" className="navbar-item-adm">
+              É administrador? Clique aqui.
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
