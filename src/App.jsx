136ea6be090577336292fb3661c159f517a5d019
EvertonWallace <everton542@hotmail.com>
2025-01-02 21:50:32 -0300

    ajustes nas rotas protegidas e publicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,58 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Cadastro from './components/cadastro/Cadastro';
 import AdminController from './components/admin/AdminController';
-import Home from './components/home/Home';
-import Places from './components/home/Places';
-import { CrudUser } from './components/crud-user/user';
-import { ProtectedRoute } from './routing/ProtectedRoute';
-import SendToken from './components/recovery/SendToken';
-import Recovery from './components/recovery/Recovery';
-import NewPassword from './components/recovery/NewPassword';
-import PasswordResetRoute from './components/recovery/PasswordResetRoute';
 import NewPlaces from './components/admin/NewPlaces';
 import UpdatePlace from './components/admin/UpdatePlace';
 import AdminHome from './components/admin/AdminHome';
-import Depoimentos from './components/home/Depoimentos.jsx';
-import DepoimentosDetails from './components/home/DepoimentosDetails.jsx';
+import { ProtectedRoute } from './routing/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<ProtectedRoute children={<Home />} />} />
-        <Route
-          path="/places"
-          element={<ProtectedRoute children={<Places />} />}
-        />
-        <Route
-          path="/depoimentos"
-          element={<ProtectedRoute children={<Depoimentos />} />}
-        />
-        <Route
-          path="/depoimentos-details"
-          element={<ProtectedRoute children={<DepoimentosDetails />} />}
-        />
+        {/* Rotas públicas */}
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
+        {/* Rotas administrativas protegidas */}
         <Route
           path="/admin"
           element={
-            <ProtectedRoute
-              redirectPath="/home"
-              children={<AdminController />}
-              adminOnly
-            />
+            <ProtectedRoute redirectPath="/login" adminOnly>
+              <AdminController />
+            </ProtectedRoute>
           }
         />
         <Route
           path="/admin/new-places"
           element={
-            <ProtectedRoute
-              redirectPath="/home"
-              children={<NewPlaces />}
-              adminOnly
-            />
+            <ProtectedRoute redirectPath="/login" adminOnly>
+              <NewPlaces />
+            </ProtectedRoute>
           }
         />
         <Route
           path="/admin/update-place"
           element={
-            <ProtectedRoute
-              redirectPath="/home"
-              children={<UpdatePlace />}
-              adminOnly
-            />
+            <ProtectedRoute redirectPath="/login" adminOnly>
+              <UpdatePlace />
+            </ProtectedRoute>
           }
         />
         <Route
           path="/admin/home"
           element={
-            <ProtectedRoute
-              redirectPath="/home"
-              children={<AdminHome />}
-              adminOnly
-            />
+            <ProtectedRoute redirectPath="/login" adminOnly>
+              <AdminHome />
+            </ProtectedRoute>
           }
         />
-        <Route
-          path="/updateUser"
-          element={<ProtectedRoute children={<CrudUser />} />}
-        />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/recovery" element={<Recovery />} />
-        <Route
-          path="/recoveryToken"
-          element={
-            <PasswordResetRoute
-              element={<SendToken />}
-              routeName="recoveryToken"
-            />
-          }
-        />
-        <Route
-          path="/newPassword"
-          element={
-            <PasswordResetRoute
-              element={<NewPassword />}
-              routeName="newPassword"
-            />
-          }
-        />
-        <Route path="/*" element={<Navigate to="/home" />} />
+
+        {/* Rota genérica para redirecionar para /home */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </BrowserRouter>
   );
