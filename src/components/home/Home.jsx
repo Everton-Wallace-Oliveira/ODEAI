0112fc44d1f274313da62017242663e646e62430
Paula Suyane <paulasuyaneprojeto2023@gmail.com>
2025-01-04 17:50:04 -0300

    tela home finalizada

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,4 @@
 import { Link } from 'react-router-dom';
-import Pelo from '../../assets/pelo.webp';
-import Image1 from '../../assets/image1.webp';
-import Image2 from '../../assets/image2.webp';
-import Image3 from '../../assets/image3.webp';
-import Image4 from '../../assets/image4.webp';
-import Image5 from '../../assets/image5.webp';
 import '../../styles/global.css';
 import '../../styles/home.css';
 import OndaNavbar from '../navbar/OndaNavbar.jsx';
@@ -22,32 +16,26 @@ function Home() {
       {/*<Header></Header>*/}
       <OndaNavbar></OndaNavbar>
 
-      <section className="section">
-        <h1 className="mainTitle">Guia histórico de Salvador</h1>
-        <p className="subtitle">
-          Venha descobrir Salvador, onde a história, a cultura e a beleza
-          natural se encontram de forma única. É um destino que promete
-          experiências inesquecíveis, desde seu centro histórico vibrante até
-          suas paisagens naturais deslumbrantes.
-        </p>
-
-        <div className="grid">
-          <img className="grid-image grid-item" src={Image1} alt="" />
-          <img className="grid-image grid-item" src={Image2} alt="" />
-          <img className="grid-image grid-item" src={Image3} alt="" />
-          <img className="grid-image grid-item" src={Image4} alt="" />
-          <img className="grid-image grid-item" src={Image5} alt="" />
-          <img className="grid-image grid-item" src={Pelo} alt="" />
-        </div>
-
-        <button className="btn-ver">
-          <Link to="/places" className="link-ver">
-            Ver mais
-          </Link>
-        </button>
-      </section>
+      <div className="containerHome">
+      <main className="content">
+        <section className="text-content">
+          <h2>Onda ODEAI</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh lectus, eleifend rutrum augue sed, consectetur rutrum est. Morbi pretium elit ut ante congue, sit amet accumsan sapien hendrerit. Donec vel velit ut mauris condimentum ornare at eget mi.</p>
+          <p>Pellentesque vehicula semper tincidunt. Maecenas sit amet viverra magna. Mauris fringilla vel justo in blandit. Nunc ut neque fermentum, varius libero quis, tempus felis.</p>
+          <p>Donec molestie, tortor vel hendrerit fringilla, massa orci sodales justo, in condimentum nisi quam ac nisl. Nunc id consequat libero.</p>
+        </section>
+        <section className="image-content">
+          <div className="image-placeholder">
+            <img src="" alt="" />
+          </div>
+        </section>
+      </main>
+    </div>
     </>
   );
 }
+
+
+
 
 export default Home;
