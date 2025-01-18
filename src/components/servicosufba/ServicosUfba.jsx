16a218faccfe1972b06ae3d27d9ee05832d29ff0
AlessandroON <122328940+AlessandroON@users.noreply.github.com>
2025-01-17 22:36:45 -0300

    Update ServicosUfba.jsx

--- src/components/servicosufba/ServicosUfba.jsx
+++ src/components/servicosufba/ServicosUfba.jsx
@@ -1,27 +1,44 @@
-import React from "react";
-import '../../styles/ServicosUfba.css';
+import React from 'react';
+import '../../styles/servicosufba.css';
+import OndaNavbar from '../navbar/OndaNavbar.jsx';
 
 function ServicosUfba() {
   return (
-    <main>
-      <h2>Serviços UFBA</h2>
-      <div className="links-section">
-        <h3>Atendimento Acadêmico</h3>
-        <a href="https://sistemas.ufba.br" target="_blank" rel="noopener noreferrer">
-          Sistemas UFBA
-        </a>
+    <div className="container">
+      <OndaNavbar />
 
-        <h3>Bibliotecas</h3>
-        <a href="https://biblioteca.ufba.br" target="_blank" rel="noopener noreferrer">
-          Biblioteca Universitária
-        </a>
+      <main>
+        <h2>Serviços UFBA</h2>
+        <div className="links-section">
+          <h3>Atendimento Acadêmico</h3>
+          <a
+            href="https://sistemas.ufba.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sistemas UFBA
+          </a>
 
-        <h3>Serviços Online</h3>
-        <a href="https://siac.ufba.br" target="_blank" rel="noopener noreferrer">
-          Portal do Aluno
-        </a>
-      </div>
-    </main>
+          <h3>Bibliotecas</h3>
+          <a
+            href="https://biblioteca.ufba.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Biblioteca Universitária
+          </a>
+
+          <h3>Serviços Online</h3>
+          <a
+            href="https://siac.ufba.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portal do Aluno
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
 
