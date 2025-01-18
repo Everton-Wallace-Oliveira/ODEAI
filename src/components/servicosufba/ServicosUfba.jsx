e8c6ca44fa87adc5b8bcd6331f0e66d269f87023
AlessandroON <122328940+AlessandroON@users.noreply.github.com>
2025-01-18 11:07:06 -0300

    Update ServicosUfba.jsx

--- src/components/servicosufba/ServicosUfba.jsx
+++ src/components/servicosufba/ServicosUfba.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/global.css';
 import '../../styles/servicosufba.css';
 import OndaNavbar from '../navbar/OndaNavbar.jsx';
+import Footer from '../footer/Footer.jsx';
 
 function ServicosUfba() {
   return (
@@ -41,6 +42,7 @@ function ServicosUfba() {
           </a>
         </div>
       </div>
+      <Footer></Footer>
     </>
   );
 }
